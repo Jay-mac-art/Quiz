3fc65ed66bca7d91c1a7f9c2cb19dfd93c4e77c4
Jay-mac-art <64605588+Jay-mac-art@users.noreply.github.com>
2021-03-14 20:38:32 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "The World Largest desert is ?",
+    answer: "Sahara",
+    options: [
+      "Sahara",
+      "Thar",
+      "Kalahari",
+      "Sonoran"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The Luna Programme was a series of unmanned space mission launched by which country?",
+    answer: "Soviet Union",
+    options: [
+      "USA",
+      "ESA",
+      "Soviet Union",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The metal whose salts are sensitive to light is ?",
+    answer: "Silver",
+    options: [
+      "Zinc",
+      "Lead",
+      "Gold",
+      "Silver"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following was the first artificial satellite?",
+    answer: "Sputnik",
+    options: [
+      "Apollo 11",
+      "Sputnik",
+      "Sojourner",
+      "Lander Beagle"
+    ]
+  },
+  
+];
